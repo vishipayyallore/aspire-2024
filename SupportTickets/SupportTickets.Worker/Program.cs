f4e8311826c26f4a3c09480b32ac4c339a2af1d1
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-12-12 21:10:01 +0530

    Update Program.cs

--- SupportTickets/SupportTickets.Worker/Program.cs
+++ SupportTickets/SupportTickets.Worker/Program.cs
@@ -2,7 +2,11 @@ using SupportTickets.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.AddAzureQueueService("QueueConnection");
+
+// Add Components before the call to AddServiceDefaults
 builder.AddServiceDefaults();
+
 builder.Services.AddHostedService<Worker>();
 
 var host = builder.Build();
