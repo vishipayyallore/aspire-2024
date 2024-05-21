6ec72d01de61a377bec2504bd6571ece71d10a25
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-21 22:17:48 +0530

    Changes

---
+++ First.AspireAppV1/First.AspireAppV1.Tests/WebTests.cs
@@ -0,0 +1,22 @@
+using System.Net;
+
+namespace First.AspireAppV1.Tests;
+
+public class WebTests
+{
+    [Fact]
+    public async Task GetWebResourceRootReturnsOkStatusCode()
+    {
+        // Arrange
+        var appHost = await DistributedApplicationTestingBuilder.CreateAsync<Projects.First.AspireAppV1_AppHost>();
+        await using var app = await appHost.BuildAsync();
+        await app.StartAsync();
+
+        // Act
+        var httpClient = app.CreateHttpClient("webfrontend");
+        var response = await httpClient.GetAsync("/");
+
+        // Assert
+        Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+    }
+}
