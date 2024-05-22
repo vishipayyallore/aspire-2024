9c705578ac554e3d1718af708aa247d90fad7fee
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-22 17:27:40 +0530

    Changes

---
+++ First.AspireApp/First.AspireApp.Web/Data/SupportTicketDbContext.cs
@@ -0,0 +1,9 @@
+﻿using First.AspireApp.Web.Data.Dtos;
+using Microsoft.EntityFrameworkCore;
+
+namespace First.AspireApp.Web.Data;
+
+public class SupportTicketDbContext(DbContextOptions options) : DbContext(options)
+{
+    public DbSet<SupportTicketDto> Tickets => Set<SupportTicketDto>();
+}
