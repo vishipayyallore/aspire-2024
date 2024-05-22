38940cf6de56e69fe4465815be4e413268e34a63
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-22 15:17:11 +0530

    Changes

--- First.AspireApp/First.AspireApp.AppHost/Program.cs
+++ First.AspireApp/First.AspireApp.AppHost/Program.cs
@@ -2,6 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
+//var storage = builder.AddAzureStorage("Storage");
+
 var apiService = builder.AddProject<Projects.First_AspireApp_ApiService>("apiservice");
 
 builder.AddProject<Projects.First_AspireApp_Web>("webfrontend")
--- First.AspireApp/TicketsStorage.Worker/Program.cs
+++ First.AspireApp/TicketsStorage.Worker/Program.cs
@@ -2,6 +2,8 @@ using TicketsStorage.Worker;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+builder.AddAzureQueueClient("QueueConnectionName");
+
 builder.AddServiceDefaults();
 builder.Services.AddHostedService<Worker>();
 
