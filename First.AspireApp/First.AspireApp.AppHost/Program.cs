46736343cfd9db4590cdf50d1b99b2cef3cd5eb8
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-05-27 15:36:06 +0530

    Changes

--- First.AspireApp/First.AspireApp.AppHost/Program.cs
+++ First.AspireApp/First.AspireApp.AppHost/Program.cs
@@ -2,15 +2,19 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
 
-//var storage = builder.AddAzureStorage("Storage");
+// Automatically provision an Application Insights resource
+var appinsights = builder.AddAzureApplicationInsights("applicationinsights");
 
-var apiService = builder.AddProject<Projects.First_AspireApp_ApiService>("apiservice");
+var apiService = builder.AddProject<Projects.First_AspireApp_ApiService>("apiservice")
+    .WithReference(appinsights);
 
 builder.AddProject<Projects.First_AspireApp_Web>("webfrontend")
     .WithExternalHttpEndpoints()
+    .WithReference(appinsights)
     .WithReference(cache)
     .WithReference(apiService);
 
-builder.AddProject<Projects.TicketsStorage_Worker>("ticketsstorage-worker");
+builder.AddProject<Projects.TicketsStorage_Worker>("ticketsstorage-worker")
+    .WithReference(appinsights);
 
 builder.Build().Run();
