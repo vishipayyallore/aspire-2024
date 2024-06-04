5dd43e4af6e8d2274b491d7bb9d37ac11872f0dc
Viswanatha Swamy <vishipayyallore@yahoo.com>
2024-06-04 12:09:54 +0530

    Swamy/27may work (#13)
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes

--- First.AspireApp/TicketsStorage.Worker/Worker.cs
+++ First.AspireApp/TicketsStorage.Worker/Worker.cs
@@ -1,14 +1,18 @@
 using Azure.Storage.Queues;
 using Azure.Storage.Queues.Models;
-using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Text.Json;
+using TicketsStorage.Worker.Data;
+using TicketsStorage.Worker.Data.Dtos;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace TicketsStorage.Worker;
 
-public class Worker(ILogger<Worker> logger, QueueServiceClient client) : BackgroundService
+public class Worker(ILogger<Worker> logger, QueueServiceClient client, IServiceProvider serviceProvider) : BackgroundService
 {
     private readonly ILogger<Worker> _logger = logger ?? throw new ArgumentNullException(nameof(logger));
-
     private readonly QueueServiceClient _client = client ?? throw new ArgumentNullException(nameof(client));
+    private readonly IServiceProvider _serviceProvider = serviceProvider ?? throw new ArgumentNullException(nameof(serviceProvider));
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
@@ -26,6 +30,31 @@ public class Worker(ILogger<Worker> logger, QueueServiceClient client) : Backgro
             {
                 _logger.LogInformation(
                     "Message from queue: {Message}", message.MessageText);
+
+                // Decode the base64-encoded message
+                string decodedMessage = Encoding.UTF8.GetString(Convert.FromBase64String(message.MessageText));
+
+                // Deserialize the JSON message into a SupportTicketDto object
+                SupportTicketDto supportTicketDto = JsonSerializer.Deserialize<SupportTicketDto>(decodedMessage)!;
+
+                // Now you can use the supportTicket object as needed
+                _logger.LogInformation("Received support ticket with title: {Title}", supportTicketDto.Title);
+
+                // Create a new scope
+                using (var scope = _serviceProvider.CreateScope())
+                {
+                    var dbContext = scope.ServiceProvider.GetRequiredService<SupportTicketDbContext>();
+
+                    // Update the support ticket in the database
+                    var supportTicket = await dbContext.Tickets.FindAsync(supportTicketDto.Id);
+                    if (supportTicket != null)
+                    {
+                        supportTicket.AssignedToName = $"Name-{Guid.NewGuid():X}";
+                        supportTicket.AssignedAt = DateTime.Now;
+                        dbContext.Tickets.Update(supportTicket);
+                        await dbContext.SaveChangesAsync();
+                    }
+                }
 
                 await queueClient.DeleteMessageAsync(
                     message.MessageId,
